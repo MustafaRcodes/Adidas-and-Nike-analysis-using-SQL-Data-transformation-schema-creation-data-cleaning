5765de329b32c35ef6ec2e2062408d35ee21c2db
MustafaR_Projects <150495517+MustafaRcodes@users.noreply.github.com>
2024-06-11 12:19:06 -0400

    Table_ rows_ import_batch processing_ process.sql

--- Table_ rows_ import_batch processing_ process.sql
+++ Table_ rows_ import_batch processing_ process.sql
@@ -1,3 +1,6 @@
+--- below is to check path where CSV file need to save or will be saved for batch processing or loading 
+SHOW VARIABLES LIKE "secure_file_priv";
+
 CREATE TABLE IF NOT EXISTS `retaildata`.`brandscsvimport` (
   `product_id` VARCHAR(50) NULL DEFAULT NULL,
   `brand` VARCHAR(50) NULL DEFAULT NULL,
@@ -9,9 +12,6 @@ COLLATE = utf8mb4_0900_ai_ci;
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Brands.csv' INTO TABLE brandscsvimport
 FIELDS TERMINATED BY ','
 IGNORE 1 ROWS;
---- below is to check path where CSV file will be saved for batch processing or loading 
-SHOW VARIABLES LIKE "secure_file_priv";
-
 
 -- To insert data into an existing table 'brandscsvimport' use the INSERT INTO statement.
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Brands.csv' 
